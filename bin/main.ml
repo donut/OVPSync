bd33eae455ed88ba995504bf4cbd2860e3c7f78d
Donovan Mueller <zotobi@gmail.com>
2018-05-02 15:41:26 -0700

    Add translation of JW source to RDB destination t.

--- bin/main.ml
+++ bin/main.ml
@@ -5,8 +5,6 @@ open Printf
 module Conf = Lib.Conf
 
 
-
-
 let main () =
   let conf = Conf.(JW_source.of_string @@ read_file "config.json") in
 
@@ -29,7 +27,7 @@ let main () =
   end) in
 
   let module Log_jw = Logger.Make(struct
-    let prefix = "JW_src"
+    let prefix = "Src"
     let level = `Trace
   end) in
 
@@ -39,24 +37,121 @@ let main () =
     let backup_expires_field = "ovp_sync.backup_expires_date"
   end) in
 
-  let module Log_pdest = Logger.Make(struct
-    let prefix = "P_dest"
+  let module Log_rdb_dest = Logger.Make(struct
+    let prefix = "Dest"
     let level = `Debug
   end) in
 
-  let module Printer_dest = struct
-    type t = string
+  let module Dest = Rdb_dest.Make(Log_rdb_dest)(struct
+    let db_pool = pool
+  end) in
 
-    let save message = 
-      Log_pdest.info message >>= fun () ->
-      Lwt.return message
-  end in
-
-  let module Synker = Sync.Make(JW_src)(Printer_dest)(struct
+  let module Synker = Sync.Make(JW_src)(Dest)(struct
     type src_t = JW_src.t
-    type dest_t = Printer_dest.t
+    type dest_t = Dest.t
 
-    let dest_t_of_src_t ((item, file, thumb) : src_t) = item.title
+    let dest_t_of_src_t ((vid, file', thumb) : src_t) =
+
+      let slug =  match BatList.assoc_opt "slug" vid.custom with
+      | Some s -> s
+      | None ->
+        BatList.assoc_opt "file_name" vid.custom |> BatOption.default vid.title
+      in
+
+      let file, width, height = match file' with
+      | None -> None, None, None 
+      | Some (f, w, h) -> Some f, w, h
+      in
+      let file_uri = BatOption.map_default Uri.of_string Uri.empty file in
+      let filename =
+        BatList.assoc_opt "file_name" vid.custom |> BatOption.default slug in
+      let duration_ptrn = Re.Perl.compile_pat "^(\\d+)(?:\\.(\\d{2}))?\\d*$" in
+      let duration = match Re.exec_opt duration_ptrn vid.duration with
+      | None -> None
+      | Some g -> match Re.Group.all g with
+        | [| ""; "" |] -> None
+        | [| i; "" |] -> Some (int_of_string i * 1000)
+        | [| i; d |] -> Some (sprintf "%s%s0" i d |> int_of_string)
+        | _ -> None
+      in
+      let thumbnail_uri = BatOption.map_default Uri.of_string Uri.empty thumb in
+
+      let tags = String.split_on_char ',' vid.tags
+        |> List.map String.trim
+      in
+
+      let cms_id = BatList.assoc_opt "RTM_site_ID" vid.custom 
+        |> BatOption.map ((^) "rightthisminute.com-")
+      in
+
+      let now = Unix.time () |> int_of_float in
+      let module Vid_j = Jw_client.Videos_video_j in
+      let author = match vid.author with
+        | None -> []
+        | Some a -> [ "author", a ]
+      in
+      let status = [ "status", vid.status |> Vid_j.string_of_media_status ] in
+      let updated = [ "updated", vid.updated |> string_of_int ] in
+      let sourcetype = [ "sourcetype", vid.sourcetype
+                                       |> Vid_j.string_of_sourcetype ] in
+      let mediatype = [ "mediatype", vid.mediatype
+                                     |> Vid_j.string_of_mediatype ] in
+      let sourceformat = match vid.sourceformat with
+      | None -> []
+      | Some s -> [ "sourceformat", s |> Vid_j.string_of_sourceformat ]
+      in
+      let size = [ "size", vid.size |> string_of_int ] in
+      let md5 = match vid.md5 with
+      | None -> []
+      | Some m -> [ "md5", m ]
+      in
+      let upload_session_id = match vid.upload_session_id with
+      | None -> []
+      | Some u -> [ "upload_session_id", u ]
+      in
+      let source_custom = 
+        [ author; status; updated; sourcetype; mediatype; sourceformat;
+          size; md5; upload_session_id ]
+        |> List.concat
+      in
+      let source = 
+        { Rdb_dest.Source. 
+          name =  "jw-" ^ Conf.JW_source.key conf
+        ; id = vid.key
+        ; created = now
+        ; updated = now
+        ; custom = source_custom }
+      in
+      
+      { Rdb_dest.Video. 
+        id = None
+
+      ; title = vid.title
+      ; slug
+
+      ; created = now
+      ; updated = now
+      ; publish = vid.date
+      ; expires = vid.expires_date
+
+      ; file_uri
+      ; filename
+      ; md5 = vid.md5
+      ; width
+      ; height
+      ; duration
+
+      ; thumbnail_uri
+      ; description = vid.description
+      ; tags = tags
+      ; custom = vid.custom
+
+      ; cms_id
+      ; link = vid.link |> BatOption.map Uri.of_string
+
+      ; canonical = source
+      ; sources = [ source ] }
+
     let should_sync _ = Lwt.return true
   end) in
 
