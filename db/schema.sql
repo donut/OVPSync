570079047e463f89bc6cb48b047cf65b3a2f5319
Donovan Mueller <zotobi@gmail.com>
2018-06-27 15:17:13 -0700

    Fix schema.sql failing foreign key restraints.

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,30 @@
--- Create syntax for TABLE 'source'
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: localhost (MySQL 5.5.5-10.2.14-MariaDB)
+# Database: ovp_sync
+# Generation Time: 2018-06-27 22:15:26 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table source
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `source`;
+
 CREATE TABLE `source` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
@@ -14,7 +40,13 @@ CREATE TABLE `source` (
   CONSTRAINT `source.video_id->video.id` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'source_field'
+
+
+# Dump of table source_field
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `source_field`;
+
 CREATE TABLE `source_field` (
   `source_id` int(11) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
@@ -25,7 +57,13 @@ CREATE TABLE `source_field` (
   CONSTRAINT `source_field.source_id->source.id` FOREIGN KEY (`source_id`) REFERENCES `source` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'tag'
+
+
+# Dump of table tag
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `tag`;
+
 CREATE TABLE `tag` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
@@ -35,7 +73,13 @@ CREATE TABLE `tag` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'variable'
+
+
+# Dump of table variable
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `variable`;
+
 CREATE TABLE `variable` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `namespace` varchar(255) NOT NULL DEFAULT '',
@@ -47,7 +91,13 @@ CREATE TABLE `variable` (
   UNIQUE KEY `namespace` (`namespace`,`key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'video'
+
+
+# Dump of table video
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `video`;
+
 CREATE TABLE `video` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(1500) NOT NULL DEFAULT '',
@@ -71,7 +121,13 @@ CREATE TABLE `video` (
   CONSTRAINT `video.canonical_source_id->source.id` FOREIGN KEY (`canonical_source_id`) REFERENCES `source` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'video_field'
+
+
+# Dump of table video_field
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `video_field`;
+
 CREATE TABLE `video_field` (
   `video_id` int(11) unsigned NOT NULL,
   `name` varchar(255) NOT NULL DEFAULT '',
@@ -82,7 +138,13 @@ CREATE TABLE `video_field` (
   CONSTRAINT `video_field.video_id->video.id` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Create syntax for TABLE 'video_tag'
+
+
+# Dump of table video_tag
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `video_tag`;
+
 CREATE TABLE `video_tag` (
   `video_id` int(11) unsigned NOT NULL,
   `tag_id` int(11) unsigned NOT NULL,
@@ -93,3 +155,13 @@ CREATE TABLE `video_tag` (
   CONSTRAINT `video_tag.tag_id->tag.id` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `video_tag.video_id->video.id` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
