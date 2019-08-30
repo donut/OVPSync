53d07007c621a567f402d0e1d5765ad62adf6cd2
Donovan Mueller <zotobi@gmail.com>
2019-08-30 11:18:24 -0700

    doc: refine

--- bin/has_changed/has_changed.mli
+++ bin/has_changed/has_changed.mli
@@ -1,9 +1,8 @@
 
-(** [video Log ~check_md5 old new'] Checks whether or not there are  
-    significnat differenes between [old] and [knew], returning [true] if there  
-    are. If [debug] is [true], then it will print out the status of each 
-    field checked. [check_md5] determines whether or not the [md5] field is 
-    checked.*)
+(** [video Log ~check_md5 old new'] Checks whether or not there are significnat 
+    differences between [old] and [new'], returning [true] if there  
+    are. [Log] is used to print out the status of each field checked.
+    [check_md5] determines whether or not the [md5] field is checked. *)
 val video 
    : (module Logger.Sig) 
   -> check_md5:bool
