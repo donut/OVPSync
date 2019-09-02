2417807ccf11a001f956d39d35035c51cf295ff8
Donovan Mueller <zotobi@gmail.com>
2019-09-02 12:25:49 -0700

    doc: add link to Dynparam in Caqti docs

--- lib/rdb_dest/dynaparam.ml
+++ lib/rdb_dest/dynaparam.ml
@@ -1,6 +1,7 @@
 
 (* A method for dynamically setting the parameters at run time.
  * @see https://github.com/paurkedal/ocaml-caqti/issues/15
+ * @see http://paurkedal.github.io/ocaml-caqti/caqti/Caqti_request/index.html#how-to-dynamically-assemble-queries-and-parameters
  *)
 
 type t = Pack : 'a Caqti_type.t * 'a * string list -> t
