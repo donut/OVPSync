202d3c1d88d2507cc991cd638a1f0dcd8179eae7
Donovan Mueller <zotobi@gmail.com>
2019-09-03 10:02:09 -0700

    fix: unused, commented out code

--- bin/has_changed/check.mli
+++ bin/has_changed/check.mli
@@ -13,41 +13,6 @@ module type Config = sig
 end
 
 
-(* module type Made = sig
-  type result
-
-  val int_field : string -> old:int -> new':int -> result
-
-  val uri_field : string -> old:Uri.t -> new':Uri.t -> result
-
-  val saveable_uri_field 
-    : string -> old:Uri.t option -> new':Uri.t option -> result
-
-  val string_field : string -> old:string -> new':string -> result
-
-  val string_list_field 
-    : string -> old:string list -> new':string list -> result
-
-  val string_pair_list_field 
-     : string -> old:(string * string) list -> new':(string * string) list 
-    -> result
-
-  val source_field
-    : string -> old:Rdb_dest.Source.t -> new':Rdb_dest.Source.t -> result
-
-  val source_list_field
-     : string -> old:Rdb_dest.Source.t list -> new':Rdb_dest.Source.t list 
-    -> result
-
-  val optional
-     : (string -> old:'value -> new':'value -> result)
-    -> string 
-    -> old:'value option
-    -> new':'value option
-    -> result
-end *)
-
-
 module Make : functor (Conf : Config) -> sig
   type result = Conf.result
 
